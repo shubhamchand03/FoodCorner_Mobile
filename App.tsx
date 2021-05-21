a315e7ba69ce997447a33f291a467b644ff44351
SHUBHAM CHAND <45413687+shubhamchand03@users.noreply.github.com>
2021-05-21 21:24:47 +0530

    MobileApp

--- App.tsx
+++ App.tsx
@@ -1,21 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import WebView from "react-native-webview";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <WebView
+      source={{ uri: "https://app-foodcorner.herokuapp.com/" }}
+      startInLoadingState={true}
+      scalesPageToFit={true}
+      style={{
+        alignContent: "center",
+        padding: 10,
+      }}
+    />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+    height: 400,
   },
 });
